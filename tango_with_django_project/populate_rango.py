b60dec0f53fae73018f8253ec8add813a4eff803
nclandrei <andreinicolae1994@yahoo.com>
2015-01-17 18:15:27 +0000

    Chapter 6 - completed

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -2,49 +2,68 @@ import os
 
 def populate():
     python_cat = add_cat(name="Python",views=128,likes=64)
+    
+    nclandrei_cat = add_cat(name="Myself",views=999,likes=999)
+
+    add_page(cat=nclandrei_cat,
+             title="GitHub nclandrei",
+             url="http://github.com/nclandrei",
+             views=5600)
+
+    add_page(cat=nclandrei_cat,
+             title="PythonAnywhere nclandrei",
+             url="https://www.pythonanywhere.com/user/nclandrei/consoles/",
+             views=86)
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",views=256)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=321)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=122)
 
     django_cat = add_cat(name="Django",views=64,likes=32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=199)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=211)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=48)
 
     frame_cat = add_cat(name="Other Frameworks",views=32,likes=16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=35)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=82)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print "- {0} - {1}".format(str(c), str(p))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)[0]
     return p
 
