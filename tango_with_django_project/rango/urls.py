86dd9f650f2dd7c50f906c7136482195dbe386d0
nclandrei <andreinicolae1994@yahoo.com>
2015-02-01 21:28:58 +0000

    Chapter 11 - completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -10,4 +10,5 @@ urlpatterns = patterns('',
         url(r'^register/$', views.register, name='register'),
         url(r'^login/$', views.user_login, name='login'),
         url(r'^restricted/', views.restricted, name='restricted'),
-        url(r'^logout/$', views.user_logout, name='logout'),)
+        url(r'^logout/$', views.user_logout, name='logout'),
+        url(r'^search/$', views.search, name='search'),)
--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -11,7 +11,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
-
+from rango.bing_search import run_query
 
 def encode_url(str):
     return str.replace(' ', '_')
@@ -286,3 +286,16 @@ def user_logout(request):
 
     # Take the user back to the homepage.
     return HttpResponseRedirect('/rango/')
+
+def search(request):
+    context = RequestContext(request)
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render_to_response('rango/search.html', {'result_list': result_list}, context)
